2f4306b3b91b23f905f64d82de04984fe8fa6a00
sergey <p956p88mhk@privaterelay.appleid.com>
2026-06-22 14:47:02 +0300

    feat: add CourseMessages constants

---
+++ src-dotnet/StudyHub.Application/Constants/CourseMessages.cs
@@ -0,0 +1,9 @@
+namespace StudyHub.Application.Constants;
+
+public static class CourseMessages
+{
+    public const string NotFound = "Курс не найден";
+    public const string CreatedSuccess = "Курс создан";
+    public const string UpdatedSuccess = "Курс обновлён";
+    public const string DeletedSuccess = "Курс удалён";
+}
