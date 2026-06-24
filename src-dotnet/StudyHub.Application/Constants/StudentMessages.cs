59ef99b2cd7ece0d4dd827c4a93e014dc341b3be
sergey <p956p88mhk@privaterelay.appleid.com>
2026-06-24 14:48:37 +0300

    feat: add StudentMessages constants

---
+++ src-dotnet/StudyHub.Application/Constants/StudentMessages.cs
@@ -0,0 +1,7 @@
+namespace StudyHub.Application.Constants;
+
+public static class StudentMessages
+{
+    public const string NotFound = "Студент не найден";
+    public const string GroupNotFound = "Группа не найдена";
+}
