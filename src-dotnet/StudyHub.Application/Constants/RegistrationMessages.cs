eab1e55032b4c3e4bfe42dbc70d95938fa307d12
sergey <p956p88mhk@privaterelay.appleid.com>
2026-06-24 11:46:04 +0300

    chore: adding a constant for registration

---
+++ src-dotnet/StudyHub.Application/Constants/RegistrationMessages.cs
@@ -0,0 +1,7 @@
+namespace StudyHub.Application.Constants;
+
+public static class AuthMessages
+{
+    public const string InvalidCredentials = "Неверный логин или пароль";
+    public const string RegistrationSuccess = "Регистрация успешна";
+}
