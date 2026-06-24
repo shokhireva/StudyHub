c39dc2db3854fa2c6d87e77e06f2fc0f994c144f
sergey <p956p88mhk@privaterelay.appleid.com>
2026-06-24 10:23:14 +0300

    feat: add PasswordHelper for SHA256 hashing

---
+++ src-dotnet/StudyHub.Application/Helpers/PasswordHelper.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StudyHub.Application.Helpers;
+
+public static class PasswordHelper
+{
+    public static string HashPassword(string password)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var hash = sha256.ComputeHash(bytes);
+        return Convert.ToBase64String(hash);
+    }
+}
